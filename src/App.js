b5e4d4f2fb069d786e948ec3854cf8678629f3bc
kelar crisp <kelcrisp007@icloud.com>
2020-05-01 17:03:43 -0700

    added react lazy to provider a fallback for slart parts of the app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { MovieContextProvider } from './Context/MovieContext';
-import HomePage from './HomePage/HomePage';
-import IndividualMovie from './IndividualMovie/IndividualMovie';
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
-import FavoriteMovies from './FavoriteMovies/FavoriteMovies';
-
-
-
+import { Spinner } from './Spinner/Spinner';
+const FavoriteMovies = React.lazy(() => import('./FavoriteMovies/FavoriteMovies')); // Lazy-
+const IndividualMovie = React.lazy(() => import('./IndividualMovie/IndividualMovie'));
+const HomePage = React.lazy(() => import('./HomePage/HomePage'));
 function App() {
   return (
     <>
-      <MovieContextProvider>
-        <BrowserRouter>
-          <Switch>
-            <FavoriteMovies path='/FavoriteMovies' component={FavoriteMovies} />
-            <IndividualMovie path='/Individual/:id' component={IndividualMovie} />
-            <HomePage path='/' component={HomePage} />
-            } />
+      <Suspense fallback={Spinner}>
+        <MovieContextProvider>
+          <BrowserRouter>
+            <Switch>
+              <FavoriteMovies path='/FavoriteMovies' component={FavoriteMovies} />
+              <IndividualMovie path='/Individual/:id' component={IndividualMovie} />
+              <HomePage path='/' component={HomePage} />
+
+              } />
           </Switch>
-        </BrowserRouter>
-      </MovieContextProvider>
+          </BrowserRouter>
+        </MovieContextProvider>
+      </Suspense>
 
     </>
   );
